c0a3789a8963386990701b6967d82bc7af558dfd
aaroniey <asilcott1@gmail.com>
2015-05-12 21:36:53 -0400

    created android studio project and now adding in the database files

---
+++ LacrosseStats/app/src/main/java/silkysmoothproductions/lacrossestats/db/PlayerTable.java
@@ -0,0 +1,52 @@
+package silkysmoothproductions.lacrossestats.db;
+
+/**
+ * Created by Asilcott on 5/12/2015.
+ */
+
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+
+public class PlayerTable {
+    // Column names
+    public static final String TABLE_COLOR = "player";
+    public static final String COLUMN_PLAYER_ID = "player_id";
+    public static final String COLUMN_FIRST_NAME = "first_name";
+    public static final String COLUMN_LAST_NAME = "last_name";
+    public static final String COLUMN_JERSEY_NUMBER = "jersey_number";
+    public static final String COLUMN_POSITION = "position";
+    public static final String COLUMN_GRADE = "grade";
+
+    // SQL statement to create the table
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_COLOR
+            + "("
+            + COLUMN_PLAYER_ID + " integer primary key autoincrement, "
+            + COLUMN_FIRST_NAME + " text not null, "
+            + COLUMN_LAST_NAME + " text not null, "
+            + COLUMN_JERSEY_NUMBER + " integer not null, "
+            + COLUMN_POSITION + " TEXT default 'none' check(position = 'fos' or position = 'lsm' or position = 'smd' or position = 'att' or position = 'mid' or position = 'gol' or position = 'def' or position = 'none' ), "
+            + COLUMN_GRADE + " TEXT default 'unknown' check(grade = 'so' or grade = 'fr' or grade = 'jr' or grade = 'sr' or grade = 'unknown') "
+            + ");";
+
+    public static void onCreate(SQLiteDatabase database) {
+
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase database,
+                                 int oldVersion,
+                                 int newVersion) {
+        Log.d(PlayerTable.class.getName(),
+                "Upgrading database from version "
+                        + oldVersion + " to " + newVersion
+                        + ", which destroyed all existing data");
+
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_COLOR);
+        onCreate(database);
+
+        Log.d("TableTask.onUpgrade()", "complete");
+    }
+}
---
+++ LacrosseStats/app/src/main/java/silkysmoothproductions/lacrossestats/db/TeamTable.java
@@ -0,0 +1,41 @@
+package silkysmoothproductions.lacrossestats.db;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+/**
+ * Created by Asilcott on 5/12/2015.
+ */
+public class TeamTable {
+    // Column names
+    public static final String TABLE_COLOR = "Team";
+    public static final String COLUMN_TEAM_ID = "team_id";
+    public static final String COLUMN_TEAM_NAME = "team_name";
+
+    // SQL statement to create the table
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_COLOR
+            + "("
+            + COLUMN_TEAM_ID + " integer primary key autoincrement, "
+            + COLUMN_TEAM_NAME + " text not null, "
+            + ");";
+
+    public static void onCreate(SQLiteDatabase database) {
+
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    public static void onUpgrade(SQLiteDatabase database,
+                                 int oldVersion,
+                                 int newVersion) {
+        Log.d(PlayerTable.class.getName(),
+                "Upgrading database from version "
+                        + oldVersion + " to " + newVersion
+                        + ", which destroyed all existing data");
+
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_COLOR);
+        onCreate(database);
+
+        Log.d("TableTask.onUpgrade()", "complete");
+    }
+}
